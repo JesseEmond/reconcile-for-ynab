adb1b21f08f0adf6bdd980c1814841e97ccb2edd
JesseEmond <emond.jesse@gmail.com>
2023-07-29 00:07:12 -0400

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+GIT_REPO_URL=$(git config --get remote.origin.url)
+
 # abort on errors
 set -e
 
@@ -13,6 +15,8 @@ cd dist
 # echo 'www.example.com' > CNAME
 
 git init
+git remote add github ${GIT_REPO_URL?}
+git checkout -b gh-pages
 git add -A
 git commit -m 'deploy'
 
@@ -20,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f 'git@github.com:JesseEmond/reconcile-for-ynab.git' 'main:gh-pages'
+git push github gh-pages --force
 
 cd -
