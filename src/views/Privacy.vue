5653c5d492bc4b217ba3e453f16edbad9cb9c5cb
dysleixa <emond.jesse@gmail.com>
2025-03-16 23:05:18 -0400

    Add disclaimer to privacy policy

--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -5,6 +5,12 @@
     </div>
     <div class="details">
       <p class="md-subheading">
+        We are not affiliated, associated, or in any way officially connected with YNAB, or any of its subsidiaries or its affiliates.
+        We are fans of YNAB and wanted to create a standalone tool to facilitate the reconciliation process and workflow, but this is fully doable in the official YNAB application on its own.
+        The official YNAB website can be found at https://www.ynab.com.
+        The names YNAB and You Need A Budget as well as related names, marks, emblems and images are registered trademarks of YNAB.
+      </p>
+      <p class="md-subheading">
         This website is fully client-side. Once you connect to your YNAB account, an access token is stored securely in your browser for a short amount of time and your browser will communicate directly with the YNAB API servers using this token to get your budget information to display it to you, as well as to create or edit transactions to reconcile your accounts.
         Your budget data is not persisted in your browser either. You can revoke this token at any point before its expiration by going in your YNAB settings.
       </p>
